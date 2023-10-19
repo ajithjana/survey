c87b0528dda0c05d7ae02ed0ffad69dbfe1fc80a
lokesh jana <lokeshjana@lokeshs-MacBook-Air-2.local>
2023-10-19 10:35:40 +0530

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,114 @@
+export const QuizData = [
+    {
+        question: "What is the primary advantage for using trucking in shipping?",
+        options:["Faster Shipping Times",
+            "Greater Flexibility of Shipping Routes",
+            "Low Transportation costs",
+            "Reduced risk of damage to goods",
+             ],
+        answer: 4
+    },
+    {
+        question: "To transport hazardous materials, what  measure  you shouldn’t prefer ?",
+        options:[ "Proper handling and storage",
+            "Ensuring Proper labelling",
+            "Overloading",
+            "Transportation regulations compliance"
+            ],
+        answer: 4
+    },
+    {
+        question: " What  qualities of a freight driver are important for safe transport?",
+        options:["Safety consciousness ",
+            "Flexibility and Adaptability",
+            "Strong Communication skills",
+            "All of the above"
+             ],
+        answer: 4
+    },
+    {
+        question: "How do you build and maintain relationships with clients and other professionals in the industry?",
+        options:[ "Communicate effectively",
+            "Improving network",
+            "Provide Excellent Service",
+           "All of the above"
+           ],
+        answer: 4
+    },
+    {
+        question: "What method of communication shouldn’t be preferred for communicating with drivers on the road  during transport?",
+        options:["Using Headlights",
+            "Using Electronic devices",
+            "Using CB radios",
+            "Using Hand Signals"
+             ],
+        answer: 4
+    },
+    {
+        question: "How do you handle the situation of delays during the transport ?",
+        options:["Plan for contingencies",
+            "Communicate with dispatch and customer",
+            "Maintain Professionalism",
+            "All of the above"
+             ],
+        answer: 4
+    },
+    {
+        question: "How do you manage your time and schedule when transporting freight? ",
+        options:["Increase the speed of the vehicle for faster transportation.",
+            "using GPS devices or routing software for finding the most efficient route",
+            "Communicate with dispatch",
+            "Both b & c",
+             ],
+        answer: 4
+    },
+    {
+        question: "How do you handle unexpected situations during transport ,such as inclement weather or road closures? ",
+        options:["Taking unnecessary risks",
+            "In bad weather conditions , it is important to prioritise safety  over speed.",
+            "Ignoring warnings or advisories",
+            "Failing to communicate with dispatch"
+            ],
+        answer: 4
+    },
+    {
+        question: "At what  speed limit would you prefer  for transporting heavy goods through a truck?(Km/hr) ",
+        options:["140-150",
+            "88-113",
+            "120 - 130",
+            "130-140"
+            ],
+        answer: 4
+    },
+    {
+        question: "Which of the following  is a potential disadvantage of using rail freight for shipping? ",
+        options:["Limited shipping Capacity",
+            "High Transportation costs",
+            "Longer Shipping Times",
+            "Increased risk of damage to goods"
+             ],
+        answer: 4
+    },
+    {
+      question:"When a vehicle is involved in a accident causing injury to any person",
+      options:["Take the vehicle to the nearest police station and report the incident",
+      "Stop the vehicle and report   to the police station",
+      "Take all reasonable steps to secure medical attention to the injured and report to the nearest place",
+      "None of the Above"
+      ],
+   answer: 3
+
+    },
+    {
+
+        questions:"What precaution must be drivers who is carrying oil tanks?",
+        options:["To avoid Alchohol","To avoid smoking","To avoid use of lights and fire while cargo is being loaded ","All of these"
+       ],
+       answer : 4
+    }
+
+
+   
+   
+
+];
